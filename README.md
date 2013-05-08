ba4d15bef6a73ca8dd5a6a623eab66187c009559
Aleksandras Nelkinas <a.nelkinas@gmail.com>
2013-05-08 14:29:02 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,78 @@
-jquery-drilldown
+jQuery Drilldown
 ================
 
-A simple jQuery plugin for creating drilldown menus
+A simple jQuery plugin for creating drilldown menus.
+
+
+Demo
+----
+
+TODO.
+
+
+Usage
+-----
+
+`$('.drilldown').drilldown()`
+
+
+HTML Structure
+--------------
+
+```html
+<div class="drilldown"><!-- required -->
+  <div class="drilldown-container"><!-- required -->
+    <ul class="drilldown-root"><!-- required, but not necessary UL -->
+      <li><a href="#">A Lorem ipsum</a></li>
+      <li>
+        <a href="#">A Dolor sit amet <span>&rarr;</span></a>
+        <ul class="drilldown-sub"><!-- required for deeper (non-root) levels -->
+          <li class="drilldown-back"><a href="#"><span>&larr;</span> Back</a></li><!-- required to be able to go back -->
+          <li><a href="#">A Fusce eget</a></li>
+          <li>
+            <a href="#">A Quam vel lorem <span>&rarr;</span></a>
+            <ul class="drilldown-sub">
+              <li class="drilldown-back"><a href="#"><span>&larr;</span> Back</a></li>
+              <li><a href="#">A Molestie tincidunt</a></li>
+              <li><a href="#">A Pellentesque</a></li>
+            </ul>
+          </li>
+          <li><a href="#">A Sit amet tincidunt</a></li>
+        </ul>
+      </li>
+      <li><a href="#">A Consectetur</a></li>
+      <li><a href="#">A Maecenas id</a></li>
+      <li>
+        <a href="#">A Hendrerit odio <span>&rarr;</span></a>
+        <ul class="drilldown-sub">
+          <li class="drilldown-back"><a href="#"><span>&larr;</span> Back</a></li>
+          <li><a href="#">A Cras tincidunt</a></li>
+          <li><a href="#">A Vivamus eu</a></li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+</div>
+```
+
+
+Options
+-------
+
+You can also pass these options (default values are shown):
+
+```javascript
+{
+  event: 'click', // *
+  selector: 'a', // *
+  speed: 100,
+  cssClass: {
+    container: 'drilldown-container',
+    root: 'drilldown-root',
+    sub: 'drilldown-sub',
+    back: 'drilldown-back'
+  }
+}
+```
+
+* `selector` and `event` will be use for binding drilldown initiating action.
---
+++ jquery.drilldown.js
@@ -0,0 +1,167 @@
+;(function ($, undefined) {
+
+  'use strict';
+
+  var pluginName = 'drilldown',
+      defaults;
+
+  defaults = {
+    event: 'click',
+    selector: 'a',
+    speed: 100,
+    cssClass: {
+      container: pluginName + '-container',
+      root: pluginName + '-root',
+      sub: pluginName + '-sub',
+      back: pluginName + '-back'
+    }
+  };
+
+  function Plugin(element, options) {
+    this._name = pluginName;
+    this._defaults = defaults;
+
+    this.element = element;
+    this.$element = $(element);
+
+    this.options = $.extend({}, defaults, options);
+
+    this.init();
+  }
+
+  Plugin.prototype = {
+
+    history: [],
+    css: {
+      float: 'left',
+      width: null
+    },
+
+    /**
+     * Initializes plugin.
+     */
+    init: function () {
+      var self = this;
+
+      this.$container = this.$element.find('.' + this.options.cssClass.container);
+      this.$element.on(this.options.event, this.options.selector, function (e) {
+        self.handleAction.call(self, e, $(this));
+      });
+    },
+
+    /**
+     * Handles user action and decides whether or not and where to drill.
+     *
+     * @param {jQuery.Event} e
+     * @param {jQuery}       $trigger
+     */
+    handleAction: function (e, $trigger) {
+      var $next = $trigger.nextAll('.' + this.options.cssClass.sub),
+          preventDefault = true;
+
+      if ($next.length) {
+        this.down.call(this, $next);
+      } else if ($trigger.closest('.' + this.options.cssClass.back).length) {
+        this.up.call(this);
+      } else {
+        preventDefault = false;
+      }
+
+      if (preventDefault && $trigger.prop('tagName') === 'A') {
+        e.preventDefault();
+      }
+    },
+
+    /**
+     * Drills down (deeper).
+     *
+     * @param {jQuery} $next
+     */
+    down: function ($next) {
+      var self = this,
+          $current;
+
+      if (!$next.length) {
+        return;
+      }
+
+      this.css.width = this.$element.outerWidth();
+      this.$container.width(this.css.width * 2);
+
+      $current = this.$container.find('.' + this.options.cssClass.root).first();
+
+      $next = $current.clone().html($next.html());
+      this.$container.append($next);
+
+      this.animateDrilling(-1 * this.css.width, function () {
+        var $current = $next.prev();
+
+        self.history.push($current);
+
+        self.restoreState.call(self, $current, $next);
+      });
+    },
+
+    /**
+     * Drills up (back).
+     */
+    up: function () {
+      var self = this,
+          $next = this.history.pop();
+
+      this.css.width = this.$element.outerWidth();
+      this.$container.width(this.css.width * 2);
+
+      this.$container.prepend($next);
+
+      this.animateDrilling(0, function () {
+        var $current = $next.next();
+
+        self.restoreState.call(self, $current, $next);
+      });
+    },
+
+    /**
+     * Animates drilling process.
+     *
+     * @param {Number}   marginLeft Target margin-left.
+     * @param {Function} callback
+     */
+    animateDrilling: function (marginLeft, callback) {
+      var $roots = this.$container.children('.' + this.options.cssClass.root);
+
+      $roots.css(this.css);
+
+      $roots.first().animate({
+        marginLeft: marginLeft
+      }, this.options.speed, callback);
+    },
+
+    /**
+     * Restores initial menu's state.
+     *
+     * @param {jQuery} $current
+     * @param {jQuery} $next
+     */
+    restoreState: function ($current, $next) {
+      $next.css({
+        float: 'none',
+        width: 'auto'
+      });
+
+      $current.remove();
+
+      this.$container.width('auto');
+    }
+
+  };
+
+  $.fn[pluginName] = function (options) {
+    return this.each(function () {
+      if (!$.data(this, pluginName)) {
+        $.data(this, pluginName, new Plugin(this, options));
+      }
+    });
+  };
+
+})(jQuery);
